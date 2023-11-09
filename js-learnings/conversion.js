dd30863ff2bfe1d9e616bd09332acc8f292579b5
Faisal Sayed <sayedfaisal544@gmail.com>
2023-11-09 23:51:01 +0530

    Adding JS to my knowledge

---
+++ js-learnings/conversion.js
@@ -0,0 +1,9 @@
+// 1 - Investigating with Number conversion
+let score = "33";
+let scoreInNumber = Number(score);
+
+let scoreInNull = null;
+let nullScoreInNumber = Number(scoreInNull);
+
+let scoreInNumberAndString = "33abc";
+let scoreInNumberAndStringNumber = Number(scoreInNumberAndString);
---
+++ js-learnings/variable-datatype.js
@@ -0,0 +1,12 @@
+const userId = 12344321;
+var userName = "Faisal";
+let userEmail = "faisal@google.com";
+let userState;
+
+/**
+ * Prefer not to use var keyword because of block scope and functional scope
+ */
+console.table([userId, userName, userEmail, userState]);
+
+console.log(typeof undefined);
+console.log(typeof null);

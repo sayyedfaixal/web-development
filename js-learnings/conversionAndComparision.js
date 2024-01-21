211b9f9060ba51dfbf951b3c31e4a7e74349783c
Faisal Sayed <sayedfaisal544@gmail.com>
2024-01-21 22:51:59 +0530

    Update the file

--- js-learnings/conversionAndComparision.js
+++ js-learnings/conversionAndComparision.js
@@ -28,13 +28,17 @@ console.log(1 + "2" + 3);
 
 console.log("" >= "");
 console.table([
+  `undefined > undefined : ${undefined > undefined}`,
+  `undefined == undefined : ${undefined == undefined}`,
   `undefined >= undefined : ${undefined >= undefined}`,
   `undefined >= null : ${undefined >= undefined}`,
   `undefined >= 1 : ${undefined >= null}`,
   `undefined >= "" : ${undefined >= ""}`,
   `undefined >= "abc" : ${undefined >= "abc"}`,
 
+  `null == null : ${null == null}`,
   `null >= null : ${null >= null}`,
+  `null > null : ${null > null}`,
   `null >= undefined : ${null >= undefined}`,
   `null >= 1 : ${null >= 1}`,
   `null >= "" : ${null >= ""}`,
@@ -44,4 +48,11 @@ console.table([
   ` "" >= undefined : ${"" >= undefined}`,
   ` "" >= 1 : ${"" >= 1}`,
   ` >= "abc" : ${"" >= "abc"}`,
+]);
+
+console.log([
+  `true : ${true}`,
+  `+true : ${+true}`,
+  `false: ${false}`,
+  `+false: ${+false}`,
 ]);
